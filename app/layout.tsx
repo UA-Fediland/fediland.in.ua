76c85fcfafe1e7b86bf6020e3f8035d88aebe5a9
Shizanit <65107731+Shizanit@users.noreply.github.com>
2025-02-17 02:33:42 +0000

    add openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,48 @@ const inter = Inter({ subsets: ["latin", "cyrillic"] })
 
 export const metadata: Metadata = {
   title: "UA Fediland",
-  description: "Український інстанс Mastodon",
+  description: "Відкритість. Децентралізація. Інновації.",
+  openGraph: {
+    type: "website",
+    url: "https://fediland.in.ua",
+    title: "UA Fediland",
+    description: "Відкритість. Децентралізація. Інновації.",
+    siteName: "UA Fediland",
+    images: [
+      {
+        url: "https://pub-855d0d5a7d064c1aa394bcd9550d15a0.r2.dev/logo.webp",
+        width: 1200,
+        height: 630,
+        alt: "UA Fediland Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "UA Fediland",
+    description: "Відкритість. Децентралізація. Інновації.",
+    images: ["https://pub-855d0d5a7d064c1aa394bcd9550d15a0.r2.dev/logo.webp"],
+  },
+  icons: {
+    icon: [
+      {
+        url: "https://pub-855d0d5a7d064c1aa394bcd9550d15a0.r2.dev/favicon.webp",
+        type: "image/webp",
+      },
+    ],
+    shortcut: [
+      {
+        url: "https://pub-855d0d5a7d064c1aa394bcd9550d15a0.r2.dev/favicon.webp",
+        type: "image/webp",
+      },
+    ],
+    apple: [
+      {
+        url: "https://pub-855d0d5a7d064c1aa394bcd9550d15a0.r2.dev/favicon.webp",
+        type: "image/webp",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
